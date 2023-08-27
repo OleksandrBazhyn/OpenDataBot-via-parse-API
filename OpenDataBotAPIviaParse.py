93c9bd2cf7380633b387531dcc374e0c206432b8
Oleksandr Bazhyn <olexandrbazhyn@gmail.com>
2023-08-27 11:56:23 +0300

    before trans to csv lib

--- OpenDataBotAPIviaParse.py
+++ OpenDataBotAPIviaParse.py
@@ -2,33 +2,83 @@
 import requests
 from bs4 import BeautifulSoup as BS
 from fake_useragent import UserAgent
+import re
+import time
 
 client = UserAgent()
 
-def soup(url):
-    response = requests.get(
-        url,
-        headers = {'client': client.random}
-    )
-    return BS(response.content, 'html.parser')
+def soup(url, max_retries=50, initial_delay=1):
+    retries = 0
+    delay = initial_delay
+    while retries < max_retries:
+        try:
+            time.sleep(delay)  # Затримка перед наступною спробою
+            response = requests.get(
+                url,
+                headers={'client': client.random},
+                cookies={}
+            )
+
+            # Перевірка статус-коду
+            if response.status_code == 200:
+                return BS(response.content, 'html.parser')
+            else:
+                print(f"Помилка при запиті. Спроба {retries + 1}/{max_retries}")
+                retries += 1
+                delay += 1  # Збільшення затримки на 1 секунду при кожній спробі
+        except Exception as e:
+            print(f"Помилка: {e}")
+    
+    print("Не вдалося виконати запит")
+    return None
     
 class OpenDataBotShortNote():
-    code = '0'
-    name = 'ТОВ "ТЮЛЬПАН"'
-    address = 'Київ'
-    phones = '+380000000000'
-    email = 'some@email.com'
-    def __init__(self, code):   #needDEBUG
+    code = ''
+    name = ''
+    address = ''
+    phones = []
+    emails = []
+    registered = False
+
+    def __init__(self, code):
         code = str(code)
         url = 'https://opendatabot.ua/c/' + code + '?from=search'
         page = soup(url)
         self.code = code
-        if(page is not None): #FIX CRITERIOUS OF FINDING STATMENTS VALUE
+        if(page is not None): #FIX CRITERIOUS OF FINDING STATMENTS VALUE?
             self.name = page.find('h1').get_text()
             self.address = page.find_all(class_='col-12 col print-responsive', limit=3)[2].find('p').get_text()
-            self.phones = page.find_all(class_='col-12 col print-responsive', limit=4)[3].find('a').get_text() #takeOnePhone(needDEBUG)
-            self.email = page.find(class_='col-sm-4 col-6 col print-responsive').find('p').get_text()
-        
+
+            # Знайдіть всі теги, де атрибут href починається з "tel:"
+            phone_links = page.find_all('a', href=re.compile(r'^tel:'))
+
+            self.phones = []
+
+            # Витягніть номери телефону з атрибутів href і додайте їх до списку
+            for link in phone_links:
+                phone_number = link['href'].replace('tel:', '')
+                self.phones.append(phone_number)
+
+            # Виведіть всі знайдені номери телефону
+            #for number in phone_numbers:
+            #    print("Phone number:", number)
+
+            #self.emails = page.find(class_='col-sm-4 col-6 col print-responsive').find('p').get_text()
+
+            records = page.find_all(class_='col-sm-4 col-6 col print-responsive')
+
+            email_pattern = re.compile(r'\S+@\S+')
+            for p in records:
+                text = p.get_text()
+                if re.search(email_pattern, text):
+                    self.emails = re.search(email_pattern, text).group()
+            
+            # Проходимо по всіх записах у масиві
+            for record in records:
+                # Перевіряємо, чи містить поточний запис слово "Зареєстровано"
+                if "Зареєстровано" in record.find('p').get_text():
+                    self.registered = True
+                    break  # Якщо знайдено, можна завершити цикл
     
 class OpenDataBotLongNote(OpenDataBotShortNote):
     updatedTime = ""
@@ -39,16 +89,3 @@ class OpenDataBotLongNote(OpenDataBotShortNote):
     typeOfActivity = ""
     VATPayer = False
     
-
-class OpenDataBotAPI():
-    def __init__(self, OpenDataBotNote):
-        pass
-    def printInfo(self, OpenDataBotNote):
-        pass
-    def getAddress(self, OpenDataBotNote):
-        return OpenDataBotNote.address
-
-    def checkByCriterios(self, OpenDataBotNote, criterio={"Київ"}):
-        return False
-    def __checkInput():
-        pass
--- OpenDataBot_API__filter_in_Kyiv_.py
+++ OpenDataBot_API__filter_in_Kyiv_.py
@@ -1,11 +1,12 @@
 # coding: cp1251
 import OpenDataBotAPIviaParse
 from bs4 import BeautifulSoup as BS
+import pandas as pd
 
 codes = []
 
 #INPUT
-with open('db\Аркуш1.html', 'r') as htmlDB:
+with open('db\input\Аркуш1.html', 'r') as htmlDB:
     contents = htmlDB.read()
     soup = BS(contents, 'html.parser')
 
@@ -13,27 +14,56 @@ with open('db\Аркуш1.html', 'r') as htmlDB:
     for el in soup:
         codes.append(el.get_text())
     codes = list(filter(None, codes))
+    
+    #DEBUG
+    print('WE GOT INPUT\n')
 
-def FilterIfItCanBeOurClient(codes):
-    checkedCodes = []
+
+# Створення порожнього DataFrame з відповідними стовпчиками
+columns = ['code', 'name', 'email', 'phones', 'address', 'registered']
+df = pd.DataFrame(columns=columns)
+#DEBUG
+print('DataFrame was created\n')
+
+def AddCompanyToDataFrame(code, name, emails, phones, address, registered):
+    phones_str = ', '.join(phones)  # З'єднати всі номери телефонів в одному рядку
+    emails_str = ', '.join(emails)  # З'єднати всі електронні пошти в одному рядку
+    new_row = pd.Series({'code': code, 'name': name, 'email': emails_str, 'phones': phones_str, 'address': address, 'registered': registered})
+    # Додати новий рядок до існуючого DataFrame
+    global df
+    df = df.append(new_row, ignore_index=True)
+    #DEBUG
+    print('company was added')
+
+def SavePotentialClients(codes):
+    global df
     for one in codes:
         # Створіть екземпляр класу OpenDataBotShortNote
         testCompany = OpenDataBotAPIviaParse.OpenDataBotShortNote(one)
+        #DEBUG
+        print(f'-----------{testCompany.code}')
 
-        # Створіть екземпляр класу OpenDataBotAPI
-        api_instance = OpenDataBotAPIviaParse.OpenDataBotAPI(testCompany)
+        if "Київ" in testCompany.address or "Київська" in testCompany.address:
+            if testCompany.registered:
+                print(testCompany.code + '\t is ' + str(testCompany.registered))
 
-        address = api_instance.getAddress(testCompany)
+                AddCompanyToDataFrame(
+                    testCompany.code,
+                    testCompany.name,
+                    testCompany.emails,
+                    testCompany.phones,
+                    testCompany.address,
+                    testCompany.registered
+                )
+                # Додайте дані до існуючого файлу CSV у режимі додавання ('a')
+                with open('db\output\output.csv', 'a', newline='') as csv_file:
+                    df.to_csv(csv_file, mode='a', header=not csv_file.tell(), index=False)
 
-        if "Київ" or "Київська" == address:
-            checkedCodes.append(testCompany.code)
+    df.to_csv('db\output\outputFromDF.csv', index=False)
+    #DEBUG
+    print('OUTPUT WAS COMPLETED')
 
-    return checkedCodes
-
-codes = FilterIfItCanBeOurClient(codes)
-
-#OUTPUT
-with open('test2.txt', 'w') as f:
-    for i in codes:
-        f.write(i + '\n')
+SavePotentialClients(codes)
+#DEBUG
+print('\nFINISH')
 
